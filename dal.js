c77bce7c4357df696a477ce76d0d3083f4c09d5a
Candace Moorer <candypattie@me.com>
2017-09-12 16:13:21 -0400

    new commit

--- dal.js
+++ dal.js
@@ -1,16 +1,2 @@
-const express = require('express')
-const session = require('express-session')
-const bodyParser = require('body-parser')
-const mustacheExpress = require('mustache-express')
-const dal = require('./dal')
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
-const app = express()
-
-app.egine('mustache', mustacheExpress())
-app.set('views', './views')
-app.set('view egine', 'mustache')
-
-app.use(bodyParser.json())
-app.use(body.Parser.urlencoded({
-  extended: true
-}))
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017/peopledb'
